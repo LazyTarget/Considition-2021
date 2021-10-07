201e11d45b16b7765f29bd36199e966d033d2ba8
Peter Åslund <peter@peteraslund.me>
2021-10-07 22:03:32 +0200

    Refactored Program.cs to use args

--- Program.cs
+++ Program.cs
@@ -9,21 +9,23 @@ namespace DotNet
 {
     public static class Program
     {
-        private const string ApiKey = "";  // TODO: Enter your API key
         // The different map names can be found on considition.com/rules
-        private const string Map = "training1";     // TODO: Enter your desired map
-        private static readonly GameLayer GameLayer = new(ApiKey);
         
         public static void Main(string[] args)
         {
-            var gameInformation = GameLayer.NewGame(Map);
-            GreedySolver greedySolver = new GreedySolver(gameInformation.Dimensions, gameInformation.Vehicle);
-            var solution = greedySolver.Solve();
-            var submitSolution = GameLayer.Submit(JsonSerializer.Serialize(solution), Map);
+            var apiKey = args[0];
+            var map = args.ElementAtOrDefault(1) ?? "training1";
+            var GameLayer = new GameLayer(apiKey);
+
+            var gameInformation = GameLayer.NewGame(map);
+            var solver = new RandomSolver(gameInformation.Dimensions, gameInformation.Vehicle);
+            var solution = solver.Solve();
+            var submitSolution = GameLayer.Submit(JsonSerializer.Serialize(solution), map);
            
             Console.WriteLine("Your GameId is: " + submitSolution.GameId);
             Console.WriteLine("Your score is: " + submitSolution.Score);
             Console.WriteLine("Link to visualisation" + submitSolution.Link);
+            Console.WriteLine("..for map: " + map);
 
         }
     }
